d0190d4114731a240d0a608016975ad060b1cd68
Javad Bargamadi <javadbargamadi1375@gmail.com>
2019-08-02 23:48:48 +0430

    override main methods with log

--- app/src/main/java/com/sematecjavaproject/logtestapp/MainActivity.java
+++ app/src/main/java/com/sematecjavaproject/logtestapp/MainActivity.java
@@ -3,6 +3,7 @@ package com.sematecjavaproject.logtestapp;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.util.Log;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +11,42 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        Log.d("CheckProcess","onCreate");
+    }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+
+        Log.d("CheckProcess","onStart");
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+
+        Log.d("CheckProcess","onResume");
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+
+        Log.d("CheckProcess", "onPause");
+    }
+
+    @Override
+    protected void onStop() {
+        super.onStop();
+
+        Log.d("CheckProcess","onStop");
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+
+        Log.d("CheckProcess","onDestroy");
     }
 }
